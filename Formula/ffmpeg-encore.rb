b872a13cf1032143a89ef0442e2321e5cc6344f7
Finn Hermansson <finn.hermansson@svt.se>
2021-04-16 11:35:09 +0200

    Bump ffmpeg, x264 and x265
    Signed-off-by: Finn Hermansson <finn.hermansson@svt.se>

--- Formula/ffmpeg-encore.rb
+++ Formula/ffmpeg-encore.rb
@@ -6,10 +6,9 @@
 class FfmpegEncore < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-4.3.1.tar.xz"
-  sha256 "ad009240d46e307b4e03a213a0f49c11b650e445b1f8be0dda2a9212b34d2ffb"
+  url "https://ffmpeg.org/releases/ffmpeg-4.4.tar.xz"
+  sha256 "06b10a183ce5371f915c6bb15b7b1fffbe046e8275099c96affc29e17645d909"
   license "GPL-2.0-only"
-  revision 1
   head "https://github.com/FFmpeg/FFmpeg.git"
   option "with-ffplay", "Enable ffplay"
 
@@ -41,7 +40,9 @@ class FfmpegEncore < Formula
   end
 
   if build.with? "ffplay"
-    depends_on "libxv" unless OS.mac?
+    on_linux do
+      depends_on "libxv"
+    end
     depends_on "sdl2"
   end
 
@@ -89,7 +90,10 @@ class FfmpegEncore < Formula
       --enable-nonfree
     ]
     args << "--enable-ffplay" if build.with? "ffplay"
-    args << "--enable-videotoolbox" if OS.mac?
+    on_macos do
+      # Needs corefoundation, coremedia, corevideo
+      args << "--enable-videotoolbox"
+    end
     system "./configure", *args
     system "make", "install"
 
