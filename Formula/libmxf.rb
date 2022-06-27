9ea90f2a068fb2a5dbca802c8cd7bec70416eefe
Gustav Grusell <gustav.grusell@svt.se>
2022-06-27 14:31:23 +0200

    Add bmx
    Signed-off-by: Gustav Grusell <gustav.grusell@svt.se>

---
+++ Formula/libmxf.rb
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2009-present, Homebrew contributors
+# SPDX-FileCopyrightText: 2022 Sveriges Television AB
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+class Libmxf < Formula
+  homepage "http://sourceforge.net/p/bmxlib/home/Home/"
+  url "https://downloads.sourceforge.net/project/bmxlib/bmx-snapshot-20210707/bmx-snapshot-20210707.tar.gz"
+  sha256 "425ec728213fd94ffc4f125b74c0597d19e210987d17ad8d7490e501a898ce5a"
+
+  env :std
+
+  depends_on "pkg-config" => :build
+  depends_on "libtool" => :build
+  on_linux do
+    depends_on "util-linux"
+  end
+
+  def install
+    Dir.chdir "libMXF"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-examples",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
---
+++ Formula/libmxfpp.rb
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2009-present, Homebrew contributors
+# SPDX-FileCopyrightText: 2022 Sveriges Television AB
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+class Libmxfpp < Formula
+  homepage "http://sourceforge.net/p/bmxlib/home/Home/"
+  url "https://downloads.sourceforge.net/project/bmxlib/bmx-snapshot-20210707/bmx-snapshot-20210707.tar.gz"
+  sha256 "425ec728213fd94ffc4f125b74c0597d19e210987d17ad8d7490e501a898ce5a"
+
+  depends_on "pkg-config" => :build
+  depends_on "libtool" => :build
+  depends_on "libmxf"
+
+  def install
+    Dir.chdir "libMXF++"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
